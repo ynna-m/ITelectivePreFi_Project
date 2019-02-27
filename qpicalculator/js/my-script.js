411f77767f1377f1a46363e7628d5995dd097c0b
niveksngry <47378047+niveksngry@users.noreply.github.com>
2019-02-27 16:10:41 +0800

    disabled units, added proto-functions

--- qpicalculator/js/my-script.js
+++ qpicalculator/js/my-script.js
@@ -1,3 +1,25 @@
+var currUnit;
+var currLetter;
+var total;
+
 var clickMe = function() {
   ons.notification.alert('oh well. Please work');
 };
+
+function concatLetter(let, x)
+{
+	currLetter = x;
+	document.getElementByID('top-input').value = document.getElementByID('top-input').value + "(" + let + " * ";
+}
+
+function concatUnit(n)
+{
+	currUnit = n;
+	total = total + (currUnit * currLetter);
+	document.getElementByID('top-input').value = document.getElementByID('top-input').value + n ") + ";
+}
+
+function sum()
+{
+	document.getElementByID('bottom-input').value = total;
+}
