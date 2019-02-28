a62ae1b94b89616193e6dfa81e65652e8934f5a7
Ynna <ynnagumban@gmail.com>
2019-02-28 14:38:27 +0800

    added clear

--- qpicalculator/js/my-script.js
+++ qpicalculator/js/my-script.js
@@ -47,3 +47,23 @@ var sum = function()
 {
 	document.getElementById('bottom-input').value = total/unitTotal;
 };
+
+var removeAll = function()
+{
+  var u = document.getElementsByClassName('unit');
+  var l = document.getElementsByClassName('letter');
+  for(var i=0; i <  u.length; i++)
+  {
+    u[i].disabled = true;
+  }
+  for(var i=0; i < l.length; i++)
+  {
+    l[i].disabled = false;
+  }
+  currUnit = 0;
+  currLetter = 0;
+  unitTotal = 0;
+  total = 0;
+  document.getElementById('top-input').value = '';
+  document.getElementById('bottom-input').value = '';
+};
